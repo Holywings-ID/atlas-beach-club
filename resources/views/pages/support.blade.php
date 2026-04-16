f1e4485602f120e6fb6711e520a2a9038a8342b4
bayu-hwg <bayu.raharjo@hwgroup.id>
2026-04-16 11:07:55 +0700

    support page

--- resources/views/pages/support.blade.php
+++ resources/views/pages/support.blade.php
@@ -111,5 +111,9 @@
                 </div>
             </div>
         </section>
+        <section class="lg:flex h-[600px]">
+            <img src="{{ asset('assets/image/family5.png') }}" alt="Map" class="w-full lg:w-2/5 object-cover" />
+            <iframe class="w-full lg:w-3/5" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3944.273392444147!2d115.13812370000001!3d-8.665529!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2dd2477f04748e85%3A0x65b83481caecd509!2sAtlas%20Beach%20Club!5e0!3m2!1sen!2sid!4v1776311801105!5m2!1sen!2sid" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+        </section>
     </main>
 </x-layouts.app>
