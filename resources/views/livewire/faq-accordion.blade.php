3c31a7b1c8d7d0b16e06cd41f298a8ea48062057
bayu-hwg <bayu.raharjo@hwgroup.id>
2026-04-21 14:56:35 +0700

    remove unused file

--- resources/views/livewire/faq-accordion.blade.php
+++
@@ -1,8 +0,0 @@
-<div class="mt-8 space-y-4">
-    @foreach ($faqs as $faq)
-        <details class="group rounded-lg border">
-            <summary class="cursor-pointer px-6 py-4 font-medium">{{ $faq['q'] }}</summary>
-            <p class="px-6 pb-4 text-sm text-gray-600">{{ $faq['a'] }}</p>
-        </details>
-    @endforeach
-</div>
